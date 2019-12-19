acf14720f21ffa4c19056dbcf3b2aaaf18df9356
chucksef <chucksef@gmail.com>
2019-12-19 12:49:45 -0700

    Now submitting via a user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def new
+    
   end
 
   def create
 
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -12,4 +14,9 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  private def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+
 end
